7a71746c5214b2e8ebafb93e546b05708983a8d1
YamilRG <eduardo_1600@hotmail.com>
2022-04-07 17:46:51 -0500

    cambio

--- sw.js
+++ sw.js
@@ -12,7 +12,6 @@ const APP_SHEll = [
 const APP_IMMUTABLE = ['https://fonts.googleapis.com/css?family=Quicksand:300,400',
 'https://fonts.googleapis.com/css?family=Lato:400,300',
 'https://use.fontawesome.com/releases/v5.3.1/css/all.cs',
-'css/animate.cs',
 'js/libs/jquery.js'];
 
 
