a0bff559910ce0725a12389f964fbeb633390b6a
YamilRG <eduardo_1600@hotmail.com>
2022-04-07 18:12:21 -0500

    cambio2

--- js/sw-acces.js
+++ js/sw-acces.js
@@ -1,9 +1,11 @@
 function actualizarCacheDinamico(dynamicCache, request, response){
     if(response.ok){
-        return caches.open(dynamicCache).then(cache =>{
-            cache.put(request, response.clone());
-            return cache.clone();
+        caches.open(dynamicCache).then(cache =>{
+        
+        cache.put(request, response.clone());
+            
         });
+        return cache.clone();
     }else{
         return response;
     }
