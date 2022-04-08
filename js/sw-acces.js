ba8688b240806f9eb741c82da3fa6e118249819d
YamilRG <eduardo_1600@hotmail.com>
2022-04-07 19:53:38 -0500

    cambio23

--- js/sw-acces.js
+++ js/sw-acces.js
@@ -4,11 +4,10 @@
 function actualizaCacheDinamico(dynamicCache, request, response){
     if(response.ok){
         //la respuesta tiene data y se debe almacaenar en cache
-        caches.open(dynamicCache).then(cache => {
-        cache.put(request,response.clone() );
-        
-            
-        });return dynamicCache;
+        caches.open(dynamicCache).then(cache =>{
+            cache.put(request, response.clone() );
+        });
+        return dynamicCache;
     }else{//si no viene nda en la respuesta
         return response;
     }
