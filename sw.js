0d3c77d37a24ee0a267366c99df840c0b92faf4e
YamilRG <eduardo_1600@hotmail.com>
2022-04-07 20:28:01 -0500

    SEPA

--- sw.js
+++ sw.js
@@ -42,13 +42,17 @@ self.addEventListener('activate', event => {
 	
 self.addEventListener('fetch', event => {
     const respuesta = caches.match(event.request).then( res => {
-        if(res){return res;}
-        else{
+        if(res){
+            return res;
+        }
+        else
+        {
             return fetch(event.request).then(newRes => {
                 return actualizaCacheDinamico(DYNAMIC_CACHE, event.request, newRes);
             });
         }
     });
+    
 });
 
 
