7b3785632e2d4e3b676848551d3a6db18f50886c
YamilRG <eduardo_1600@hotmail.com>
2022-04-07 19:31:08 -0500

    cambio2

--- sw.js
+++ sw.js
@@ -9,6 +9,7 @@ const APP_SHEll = [
     'img/avs/Debian.png', 
     'js/app.js', 
     'js/sw-acces.js']; 
+    
 const APP_IMMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
