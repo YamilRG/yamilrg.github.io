af9f901f77b9d8c12ccddda9bd36b1b099717ddd
YamilRG <eduardo_1600@hotmail.com>
2022-04-07 19:27:30 -0500

    cambiomas

--- sw.js
+++ sw.js
@@ -10,10 +10,10 @@ const APP_SHEll = [
     'js/app.js', 
     'js/sw-acces.js']; 
 const APP_IMMUTABLE = [
-'https://fonts.googleapis.com/css?family=Quicksand:300,400',
-'https://fonts.googleapis.com/css?family=Lato:400,300',
-'https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css',
-'js/libs/jquery.js'];
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css',
+    'js/libs/jquery.js'];
 
 
 
